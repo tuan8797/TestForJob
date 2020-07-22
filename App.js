ec7be9bb228f316f6a800c9157f82f364b721464
Nguyễn Tuấn <tuan8797@gmail.com>
2020-07-22 21:21:17 +0700

    circle progress

--- App.js
+++ App.js
@@ -7,14 +7,47 @@
  */
 
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 import MainScreen from './src/screen/mainScreen';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 const App: () => React$Node = () => {
   return (
     <>
-      <SafeAreaView>
-        <MainScreen />
+      <SafeAreaView style={{flex: 1}}>
+      <AnimatedCircularProgress
+          size={200}
+          width={3}
+          fill={100}
+          duration={5650000}
+          tintColor="#00e0ff"
+          backgroundColor="#3d5875">
+          {
+            (fill) => 
+            {
+              const a = (60 * fill) / 100
+              return (
+              <Text>
+                {a}
+              </Text>
+            )}
+          }
+        </AnimatedCircularProgress>
+        <AnimatedCircularProgress
+          size={200}
+          width={3}
+          fill={100}
+          duration={60000}
+          tintColor="#00e0ff"
+          backgroundColor="#3d5875">
+          {
+            (fill) => (
+              <Text>
+                {Math.round((60 * fill) / 100)}
+              </Text>
+            )
+          }
+        </AnimatedCircularProgress>
       </SafeAreaView>
     </>
   );
